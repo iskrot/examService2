18133820205e6f9267946301dce8251de269e198
iskrot <olga.volwskaya@gmail.com>
2025-05-27 14:04:21 +0300

    create

---
+++ src/main/java/org/skypro/questionGenerate/QuestionGenerateApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.questionGenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionGenerateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/questionGenerate/QuestionGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.questionGenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
